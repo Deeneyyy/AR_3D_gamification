767cc2e87f4e10bdb9289c637bce60ce96687e85
Dinesh A <deendinesh619@gmail.com>
2025-03-12 17:35:32 +0530

    non RGB snaping

--- Assets/v2/DragDrop.cs
+++ Assets/v2/DragDrop.cs
@@ -1,58 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+ 
 public class DragDrop : MonoBehaviour
 {
-    private Vector3 offset;
-    private Vector3 startPosition; // Store the original position
-    public string correctDestinationTag; // Assign the correct drop area tag in the Inspector
-
-    void Start()
-    {
-        startPosition = transform.position; // Save the initial position
-    }
-
+    Vector3 offset;
+    public string destinationTag = "DropArea";
+ 
     void OnMouseDown()
     {
-        offset = transform.position - GetMouseWorldPosition();
-        GetComponent<Collider>().enabled = false;
+        offset = transform.position - MouseWorldPosition();
+        transform.GetComponent<Collider>().enabled = false;
     }
-
+ 
     void OnMouseDrag()
     {
-        transform.position = GetMouseWorldPosition() + offset;
+        transform.position = MouseWorldPosition() + offset;
     }
-
+ 
     void OnMouseUp()
     {
-        GetComponent<Collider>().enabled = true;
-
         var rayOrigin = Camera.main.transform.position;
-        var rayDirection = GetMouseWorldPosition() - Camera.main.transform.position;
+        var rayDirection = MouseWorldPosition() - Camera.main.transform.position;
         RaycastHit hitInfo;
-
-        if (Physics.Raycast(rayOrigin, rayDirection, out hitInfo))
+        if(Physics.Raycast(rayOrigin, rayDirection, out hitInfo))
         {
-            // Check if the cube is placed in the correct region
-            if (hitInfo.transform.CompareTag(correctDestinationTag))
+            if(hitInfo.transform.tag == destinationTag)
             {
-                transform.position = hitInfo.transform.position; // Snap to the correct area
-            }
-            else
-            {
-                transform.position = startPosition; // Return to original position if incorrect
+                transform.position = hitInfo.transform.position;
             }
         }
-        else
-        {
-            transform.position = startPosition; // Return to original position if no valid drop
-        }
+        transform.GetComponent<Collider>().enabled = true;
     }
-
-    private Vector3 GetMouseWorldPosition()
+ 
+    Vector3 MouseWorldPosition()
     {
-        Vector3 mouseScreenPos = Input.mousePosition;
+        var mouseScreenPos = Input.mousePosition;
         mouseScreenPos.z = Camera.main.WorldToScreenPoint(transform.position).z;
         return Camera.main.ScreenToWorldPoint(mouseScreenPos);
     }
